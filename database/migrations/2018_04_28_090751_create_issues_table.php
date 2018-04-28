0f036538f45f59fde6d9c9cb0a7f054a625a4068
Nasirou Wagana <wnasirou@gmail.com>
2018-04-28 10:15:06 +0100

    First commit

---
+++ database/migrations/2018_04_28_090751_create_issues_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIssuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('status', 15)->default('pending');
+            $table->string('submitter_telephone', 45);
+            $table->string('submitter_email', 100)->nullable();
+            $table->string('submitter_ip', 15)->nullable();
+            $table->string('address', 255)->nullable();
+            $table->unsignedInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issues');
+    }
+}
